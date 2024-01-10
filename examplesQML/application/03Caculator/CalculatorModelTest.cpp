a8dde264b1ff5402bd743909667656fe12b0bd75
avillanuevao <150674274+avillanuevao@users.noreply.github.com>
2024-01-10 08:49:20 +0100

    Add files via upload

---
+++ examplesQML/application/03Caculator/CalculatorModelTest.cpp
@@ -0,0 +1,30 @@
+#include "CalculatorModel.hpp"
+#include <iostream>
+
+//void runTests() {
+//    CalculatorModel calculator;
+
+//    // Test Addition
+//    calculator.setInput(5.0);
+//    calculator.setInput(CalculatorComponent::ADD);
+//    calculator.setInput(3.0);
+//    calculator.setInput(CalculatorComponent::EQUAL);
+//    assert(calculator.getDisplayText() == "8");
+
+//    // Test Subtraction
+//    calculator.setInput(10.0);
+//    calculator.setInput(CalculatorComponent::SUBTRACT);
+//    calculator.setInput(4.0);
+//    calculator.setInput(CalculatorComponent::EQUAL);
+//    assert(calculator.getDisplayText() == "6");
+
+//    // Add more tests for other operations and cases
+
+//    // Test Clear
+//    calculator.setInput(5.0);
+//    calculator.setInput(CalculatorComponent::CLEAR);
+//    assert(calculator.getDisplayText().isEmpty());
+
+//    std::cout << "All tests passed successfully." << std::endl;
+//}
+
